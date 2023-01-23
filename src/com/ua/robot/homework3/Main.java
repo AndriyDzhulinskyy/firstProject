a8d90c60509fbc88fafba87ef1e0bbc7aefc3b25
Andriy Dzhulinskyy <dav784@ukr.net>
2023-01-23 19:30:28 +0200

    work done

---
+++ src/com/ua/robot/homework3/Main.java
@@ -0,0 +1,15 @@
+package com.ua.robot.homework3;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 1;
+        System.out.println("a = " + a + "   b = " + b);
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+
+    }
+}
