ca3369e48888c8b29d489db441128a822e80b689
Andriy Dzhulinskyy <dav784@ukr.net>
2023-01-23 22:47:33 +0200

    work done

---
+++ src/com/ua/robot/homework4/Main.java
@@ -0,0 +1,12 @@
+package com.ua.robot.homework4;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String str = "HVhjvJHVJHVjhvJHCJcjCJGCG";
+        System.out.println(str.toLowerCase());
+        System.out.println(str.toUpperCase());
+        System.out.println(str.charAt(0));
+        System.out.println(str.charAt(str.length()-1));
+    }
+}
